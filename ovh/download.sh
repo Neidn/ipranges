2a29ac7e4a07fe77922e121e6707fcd7e50a34b7
Neidn <dty3152@gmail.com>
2025-01-02 15:21:30 +0900

    modify scripts to use json types not xml

--- ovh/download.sh
+++ ovh/download.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
 
 set -exuo pipefail
+set -x
 
-OVH_URL="https://rest.db.ripe.net/search?source=ripe&query-string=OVH&select-attributes=inetnum&flags=no-referenced&flags=no-irt&flags=no-filtering"
+ASN="AS16276"
+OVH_URL="https://rest.db.ripe.net/search?inverse-attribute=origin&source=ripe&query-string=${ASN}"
 
+PWD=`pwd`
 TMP_DIR="$(mktemp -d)"
-TMP_FILE="${TMP_DIR}/ovh.txt"
+TMP_FILE="ovh.txt"
+TMP_IPv4_FILE="${TMP_DIR}/ovh-ipv4.txt"
+TMP_IPv6_FILE="${TMP_DIR}/ovh-ipv6.txt"
 
-TARGET_DIR="ovh"
-TARGET_FILE="${TARGET_DIR}/ovh-ipv4.txt"
+TARGET_DIR="${PWD}/ovh"
+IPv4_TARGET_FILE="${TARGET_DIR}/ovh-ipv4.txt"
+IPv6_TARGET_FILE="${TARGET_DIR}/ovh-ipv6.txt"
 
 # Download the xml file
-curl -s "${OVH_URL}" | xmllint --xpath '//attribute[@name="inetnum"]/@value' - | awk -F'"' '{print $2}' > "${TMP_FILE}"
+cd "${TMP_DIR}"
+curl -H 'Accept: application/json' -s "${OVH_URL}" > "${TMP_FILE}"
 
-# Extract and calculate the ipv4 subnets
-cat "${TMP_FILE}" | while read line; do
-  command="ipcalc ${line}"
-  result=`$command | grep -v deaggregate`
-  echo "$result" >> "${TARGET_FILE}"
-done
+# Extract the IPv4 and IPv6 addresses
+duckdb -c "COPY(WITH parsed_data AS (select unnest(json.objects.object) as object from read_json('${TMP_FILE}') json) SELECT object->'$.primary-key'->'$.attribute[0]'->'$.value' as list FROM parsed_data where object.type = 'route') TO '${TMP_IPv4_FILE}' (FORMAT 'csv', HEADER false, QUOTE '')"
+duckdb -c "COPY(WITH parsed_data AS (select unnest(json.objects.object) as object from read_json('${TMP_FILE}') json) SELECT object->'$.primary-key'->'$.attribute[0]'->'$.value' as list FROM parsed_data where object.type = 'route6') TO '${TMP_IPv6_FILE}' (FORMAT 'csv', HEADER false, QUOTE '')"
 
-# Sort the files
-sort -V "${TARGET_FILE}" -o "${TARGET_FILE}"
+# Remove Quotes
+cd "${PWD}"
+sed -i 's/"//g' "${TMP_IPv4_FILE}"
+sed -i 's/"//g' "${TMP_IPv6_FILE}"
+
+# Sort and remove duplicates
+sort -V "${TMP_IPv4_FILE}" | uniq > "${IPv4_TARGET_FILE}"
+sort -V "${TMP_IPv6_FILE}" | uniq > "${IPv6_TARGET_FILE}"
 
 # Clean up
 rm -rf "${TMP_DIR}"
